843f479be08e0d6ad0454751f3b943faff71ac01
Sophie Traen <sophie_traen@yahoo.com>
2021-10-06 09:56:51 +0200

    added custom style sheet

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
+import "!style-loader!css-loader!./styles.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -11,4 +12,4 @@ export const parameters = {
     },
   },
   docs: { inlineStories: true },
-}
+};
